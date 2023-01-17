b853a7b1272f6c0bf08af8e89260e616cffaf62e
SUNDUS-THE-CODER <119600559+SUNDUS-THE-CODER@users.noreply.github.com>
2023-01-17 22:12:14 +0000

    add all required functions

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+const destructivelyAppendCat = (name) => cats.push(name);
+const destructivelyPrependCat = (name) => cats.unshift(name);
+const destructivelyRemoveLastCat = () => cats.pop();
+const destructivelyRemoveFirstCat = () => cats.shift();
+const appendCat = (name) => [...cats, name];
+const prependCat = (name) => [name, ...cats];
+const removeLastCat = () => cats.filter((key) => cats.indexOf(key) !== cats.length - 1);
+const removeFirstCat = () => cats.filter((key) => cats.indexOf(key) !== 0);
